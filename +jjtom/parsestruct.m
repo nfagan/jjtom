f455727f58a1158c7fa2f0a3d1fe29e12ff03f90
nfagan <nick@fagan.org>
2019-01-16 11:00:07 -0500

    convert parsestruct -> shared_utils version

--- +jjtom/parsestruct.m
+++ +jjtom/parsestruct.m
@@ -1,18 +1,5 @@
-function params = parsestruct(params,args)
+function params = parsestruct(params, args)
 
-names = fieldnames(params);
+params = shared_utils.general.parsestruct( params, args );
 
-nArgs = length(args);
-
-if round(nArgs/2)~=nArgs/2
-   error('Name-value pairs are incomplete!')
-end
-
-for pair = reshape(args,2,[])
-   inpName = pair{1};
-   if any(strcmp(inpName,names))
-      params.(inpName) = pair{2};
-   else
-      error('%s is not a recognized parameter name',inpName)
-   end
 end
